7a50d18ce4ecb5c03c8a1ae5ccb71c6c9b7c359e
jeffreyoldenburger <jeffreyoldenburger@jeffreys-mbp.groningen.nl.ibm.com>
2016-10-11 15:18:03 +0200

    completed achievement Catg and Count

--- src/com/ibm/achievement/dao/model/AchievementCount.java
+++ src/com/ibm/achievement/dao/model/AchievementCount.java
@@ -1,5 +1,43 @@
 package com.ibm.achievement.dao.model;
 
-public class AchievementCount {
+public class AchievementCount extends java.lang.Object{
 
+	private String CurrentMonthCount;
+	private String	CurrentYearCount;
+	private String	EmployeeId;
+
+	public AchievementCount(){
+		
+	}
+
+	public String getCurrentMonthCount() {
+		return CurrentMonthCount;
+	}
+
+	public void setCurrentMonthCount(String currentMonthCount) {
+		CurrentMonthCount = currentMonthCount;
+	}
+
+	public String getCurrentYearCount() {
+		return CurrentYearCount;
+	}
+
+	public void setCurrentYearCount(String currentYearCount) {
+		CurrentYearCount = currentYearCount;
+	}
+
+	public String getEmployeeId() {
+		return EmployeeId;
+	}
+
+	public void setEmployeeId(String employeeId) {
+		EmployeeId = employeeId;
+	}
+	
+	
+	
+	
+	
+	
+	
 }
