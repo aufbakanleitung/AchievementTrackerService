c95fdaf0162c8d9ef6ef869df80ba10be965042d
thifane <thifane@thifanes-mbp.groningen.nl.ibm.com>
2016-10-11 14:23:28 +0200

    Created util class

---
+++ src/com/ibm/achievement/dao/util/AchievementListeners.java
@@ -0,0 +1,29 @@
+package com.ibm.achievement.dao.util;
+
+public class AchievementListeners extends java.lang.Object {
+
+	private String AchievementManager;
+	private String EmployeeManager;
+	private String ProjectManager;
+	private String UserManager;
+	
+	public AchievementListeners() {
+	}
+
+	public String getAchievementManager() {
+		return AchievementManager;
+	}
+
+	public String getEmployeeManager() {
+		return EmployeeManager;
+	}
+
+	public String getProjectManager() {
+		return ProjectManager;
+	}
+
+	public String getUserManager() {
+		return UserManager;
+	}
+
+}
