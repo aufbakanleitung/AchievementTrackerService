d1a3b089d89f6e4908b7e57afdd4bc807c286a8c
jeffreyoldenburger <jeffreyoldenburger@jeffreys-mbp.groningen.nl.ibm.com>
2016-10-11 14:38:00 +0200

    dao model achievement fixed

--- src/com/ibm/achievement/dao/model/Achievement.java
+++ src/com/ibm/achievement/dao/model/Achievement.java
@@ -1,47 +1,130 @@
 package com.ibm.achievement.dao.model;
 
+import java.util.Date;
+import java.util.List;
+
 public class Achievement extends java.lang.Object{
-
-
-	
-	-----------
-	
-	getAchievementDoc() 
     
-	private int AchievementId();
-	private String	AchievementText();
-	private String	AchievementTypeId();
-	private String	ApproverComment();
-	private String	getApproverId();
-	private int ApproverPointVal();
-	private String CategoryId();
-	private String	getEmployeeId();
-	private Date EndDate();
-	private String ProjectId();
-	private Date StartDate();
-	private String StatusCode();
-	private void AchievementDoc(java.util.List<AchievementDoc> achievementDoc);
-	private void AchievementId(int achievementId);
-	private void setAchievementText(String achievementText);
-	private void AchievementTypeId(java.lang.String achievementTypeId) 
-           
- void	setApproverComment(java.lang.String approverComment) 
-           
- void	setApproverId(java.lang.String approverId) 
-           
- void	setApproverPointVal(int approverPointVal) 
-           
- void	setCategoryId(java.lang.String categoryId) 
-           
- void	setEmployeeId(java.lang.String employeeId) 
-           
- void	setEndDate(java.util.Date endDate) 
-           
- void	setProjectId(java.lang.String projectId) 
-           
- void	setStartDate(java.util.Date startDate) 
-           
- void	setStatusCode(java.lang.String statusCode) 
-           
+	private List AchievementDoc;
+	private int AchievementId;
+	private String	AchievementText;
+	private String	AchievementTypeId;
+	private String	ApproverComment;
+	private String	getApproverId;
+	private int ApproverPointVal;
+	private String CategoryId;
+	private String	getEmployeeId;
+	private Date EndDate;
+	private String ProjectId;
+	private Date StartDate;
+	private String StatusCode;
+	
+	public Achievement(){
+	}
+
+	public List getAchievementDoc() {
+		return AchievementDoc;
+	}
+
+	public void setAchievementDoc(List achievementDoc) {
+		AchievementDoc = achievementDoc;
+	}
+
+	public int getAchievementId() {
+		return AchievementId;
+	}
+
+	public void setAchievementId(int achievementId) {
+		AchievementId = achievementId;
+	}
+
+	public String getAchievementText() {
+		return AchievementText;
+	}
+
+	public void setAchievementText(String achievementText) {
+		AchievementText = achievementText;
+	}
+
+	public String getAchievementTypeId() {
+		return AchievementTypeId;
+	}
+
+	public void setAchievementTypeId(String achievementTypeId) {
+		AchievementTypeId = achievementTypeId;
+	}
+
+	public String getApproverComment() {
+		return ApproverComment;
+	}
+
+	public void setApproverComment(String approverComment) {
+		ApproverComment = approverComment;
+	}
+
+	public String getGetApproverId() {
+		return getApproverId;
+	}
+
+	public void setGetApproverId(String getApproverId) {
+		this.getApproverId = getApproverId;
+	}
+
+	public int getApproverPointVal() {
+		return ApproverPointVal;
+	}
+
+	public void setApproverPointVal(int approverPointVal) {
+		ApproverPointVal = approverPointVal;
+	}
+
+	public String getCategoryId() {
+		return CategoryId;
+	}
+
+	public void setCategoryId(String categoryId) {
+		CategoryId = categoryId;
+	}
+
+	public String getGetEmployeeId() {
+		return getEmployeeId;
+	}
+
+	public void setGetEmployeeId(String getEmployeeId) {
+		this.getEmployeeId = getEmployeeId;
+	}
+
+	public Date getEndDate() {
+		return EndDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		EndDate = endDate;
+	}
+
+	public String getProjectId() {
+		return ProjectId;
+	}
+
+	public void setProjectId(String projectId) {
+		ProjectId = projectId;
+	}
+
+	public Date getStartDate() {
+		return StartDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		StartDate = startDate;
+	}
+
+	public String getStatusCode() {
+		return StatusCode;
+	}
+
+	public void setStatusCode(String statusCode) {
+		StatusCode = statusCode;
+	}
+	
 	
 }
