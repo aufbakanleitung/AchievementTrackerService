88f64799bd390e1c636cd8afe3a6dbabd6200341
thifane <thifane@thifanes-mbp.groningen.nl.ibm.com>
2016-10-11 15:30:23 +0200

    Created project manager and user manager classes in achievement model

--- src/com/ibm/achievement/dao/manager/UserManager.java
+++ src/com/ibm/achievement/dao/manager/UserManager.java
@@ -1,5 +1,52 @@
 package com.ibm.achievement.dao.manager;
 
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import com.ibm.achievement.dao.model.User;
+
+@Component
 public class UserManager {
+	
+public UserManager(){
+	
+}
+
+@Transactional
+public int deleteUserData(String mailId) throws java.sql.SQLException {
+	return 0;
+}
+
+@Transactional
+public int insertUserData (String mailId, String passwd, String activeFlg, String role)
+		throws java.sql.SQLException {
+	
+	return 0;
+	
+}
+
+@Transactional
+public int updateActiveFlag (String mailId, String activeFlg)
+		throws java.sql.SQLException {
+	
+	return 0;
+}
+
+public List<User> findAllUser() throws java.sql.SQLException {
+
+	return null;
+}
+
+public List<User> findUserByActiveFlg (String actFlg) throws java.sql.SQLException {
+	
+	return null;
+}
+
+public User findUserbyMailId (String mailId) throws java.sql.SQLException {
+	
+	return null;
+}
+
 
 }
