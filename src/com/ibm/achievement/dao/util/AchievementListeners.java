b32962e1f7a6393483b797378923724166500651
Herman van der Veer <hermanvanderveer@hermans-mbp.groningen.nl.ibm.com>
2016-10-12 15:47:02 +0200

    uncommented @component in AchievementListerers.

--- src/com/ibm/achievement/dao/util/AchievementListeners.java
+++ src/com/ibm/achievement/dao/util/AchievementListeners.java
@@ -1,14 +1,12 @@
 package com.ibm.achievement.dao.util;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 import com.ibm.achievement.dao.manager.AchievementManager;
 import com.ibm.achievement.dao.manager.EmployeeManager;
 import com.ibm.achievement.dao.manager.ProjectManager;
 import com.ibm.achievement.dao.manager.UserManager;
 
-@Component
 public class AchievementListeners {
 
 	@Autowired
